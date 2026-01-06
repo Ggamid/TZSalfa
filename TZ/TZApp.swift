edf8347b0d8f04563fd39d9edc2c8621098b0699
Гамид Халидов <khalidov-gamid@mail.ru>
2026-01-06 13:35:00 +0300

    initial commit

---
+++ TZ/TZApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZApp.swift
+//  TZ
+//
+//  Created by Gamıd Khalıdov on 06.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
