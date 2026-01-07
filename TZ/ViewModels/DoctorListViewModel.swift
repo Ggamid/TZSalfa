d5e4fe92d34a6d97f592af2c03efc0de285769ea
Гамид Халидов <khalidov-gamid@mail.ru>
2026-01-07 17:45:47 +0300

    DoctorListViewModel realized

--- TZ/ViewModels/DoctorListViewModel.swift
+++ TZ/ViewModels/DoctorListViewModel.swift
@@ -5,3 +5,15 @@
 //  Created by Gamıd Khalıdov on 06.01.2026.
 //
 
+import SwiftUI
+
+class DoctorListViewModel: ObservableObject {
+    @Published var doctors: [Doctor] = []
+    let networkService = NetworkService()
+    
+    func getDoctorList() {
+        if let doctorResponse = networkService.loadJSONFromBundle("doctor", as: DoctorsResponse.self) {
+            self.doctors = doctorResponse.data.users
+        }
+    }
+}
