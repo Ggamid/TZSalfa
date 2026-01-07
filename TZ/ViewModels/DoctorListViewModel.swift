da1efbe22718bc6dbced7505b60eb3454d82fe9d
Гамид Халидов <khalidov-gamid@mail.ru>
2026-01-07 19:23:29 +0300

    filter support added to ListViewModel

--- TZ/ViewModels/DoctorListViewModel.swift
+++ TZ/ViewModels/DoctorListViewModel.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 class DoctorListViewModel: ObservableObject {
     @Published var doctors: [Doctor] = []
+    @Published var filter: DoctorFilter = .price
+    
     let networkService = NetworkService()
+    
+    var filteredList: [Doctor] {
+        switch filter {
+        case .price:
+            return doctors.sorted { $0.minServicePrice < $1.minServicePrice }
+        case .experience:
+            return doctors.sorted { $0.seniority < $1.seniority }
+        case .rating:
+            return doctors.sorted { $0.rank < $1.rank }
+        }
+    }
     
     func getDoctorList() {
         if let doctorResponse = networkService.loadJSONFromBundle("doctor", as: DoctorsResponse.self) {
