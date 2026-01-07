f2167110444d8860ecad480922f2ada6314957f7
Гамид Халидов <khalidov-gamid@mail.ru>
2026-01-07 21:42:47 +0300

    DoctorListViewModel refactoring

--- TZ/ViewModels/DoctorListViewModel.swift
+++ TZ/ViewModels/DoctorListViewModel.swift
@@ -12,35 +12,51 @@ class DoctorListViewModel: ObservableObject {
     @Published var filter: DoctorFilter = .priceAsc
     @Published var searchText: String = ""
     
-    let networkService = NetworkService()
+    private let networkService: NetworkServiceProtocol
+    
+    init(networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
+    }
     
     var filteredList: [Doctor] {
-        
-        var filteredList = doctors
-        
-        if searchText != "" {
-            filteredList = filteredList.filter { $0.doctorFullName.localizedStandardContains(searchText) || $0.doctorSpecializations.localizedStandardContains(searchText) }
-        }
-        
-        switch filter {
-        case .priceAsc:
-            return filteredList.sorted { $0.minServicePrice < $1.minServicePrice }
-        case .priceDesc:
-            return filteredList.sorted { $0.minServicePrice > $1.minServicePrice }
-        case .experienceAsc:
-            return filteredList.sorted { $0.seniority < $1.seniority }
-        case .experienceDesc:
-            return filteredList.sorted { $0.seniority > $1.seniority }
-        case .ratingAsc:
-            return filteredList.sorted { $0.rank < $1.rank }
-        case .ratingDesc:
-            return filteredList.sorted { $0.rank > $1.rank }
-        }
+        let searched = applySearch(on: doctors)
+        return sort(searched, by: filter)
     }
     
     func getDoctorList() {
         if let doctorResponse = networkService.loadJSONFromBundle("doctor", as: DoctorsResponse.self) {
             self.doctors = doctorResponse.data.users
+        }
+    }
+}
+
+private extension DoctorListViewModel {
+    var trimmedQuery: String {
+        searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    func applySearch(on list: [Doctor]) -> [Doctor] {
+        guard !trimmedQuery.isEmpty else { return list }
+        return list.filter {
+            $0.doctorFullName.localizedStandardContains(trimmedQuery) ||
+            $0.doctorSpecializations.localizedStandardContains(trimmedQuery)
+        }
+    }
+    
+    func sort(_ list: [Doctor], by filter: DoctorFilter) -> [Doctor] {
+        switch filter {
+        case .priceAsc:
+            return list.sorted { $0.minServicePrice < $1.minServicePrice }
+        case .priceDesc:
+            return list.sorted { $0.minServicePrice > $1.minServicePrice }
+        case .experienceAsc:
+            return list.sorted { $0.seniority < $1.seniority }
+        case .experienceDesc:
+            return list.sorted { $0.seniority > $1.seniority }
+        case .ratingAsc:
+            return list.sorted { $0.rank < $1.rank }
+        case .ratingDesc:
+            return list.sorted { $0.rank > $1.rank }
         }
     }
 }
