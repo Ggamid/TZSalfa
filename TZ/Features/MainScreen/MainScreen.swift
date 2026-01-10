e118e0578785a6ba7b6b563a25cc60b0dc25f8ca
Гамид Халидов <khalidov-gamid@mail.ru>
2026-01-10 21:08:47 +0300

    custom tabbar with switch -> TabView + .hidden + TabBar

--- TZ/Features/MainScreen/MainScreen.swift
+++ TZ/Features/MainScreen/MainScreen.swift
@@ -9,20 +9,41 @@ import SwiftUI
 
 struct MainScreen: View {
     
-    @State var appTab: AppTab = .home
-    
+    @State private var appTab: AppTab = .home
+
     var body: some View {
-        VStack(spacing: 0) {
-            switch appTab {
-            case .home:
+        ZStack(alignment: .bottom) {
+            TabView(selection: $appTab) {
                 DoctorListView()
-            default:
-                Spacer()
+                    .tabItem {
+                        Label(AppTab.home.title, image: AppTab.home.imageName)
+                    }
+                    .tag(AppTab.home)
+
+                Text("Приёмы")
+                    .tabItem {
+                        Label(AppTab.calendar.title, image: AppTab.calendar.imageName)
+                    }
+                    .tag(AppTab.calendar)
+
+                Text("Чат")
+                    .tabItem {
+                        Label(AppTab.chat.title, image: AppTab.chat.imageName)
+                    }
+                    .tag(AppTab.chat)
+
+                Text("Профиль")
+                    .tabItem {
+                        Label(AppTab.profile.title, image: AppTab.profile.imageName)
+                    }
+                    .tag(AppTab.profile)
             }
-            
+            .toolbar(.hidden, for: .tabBar)
+
             TabBarView(selected: $appTab, chatBadge: 1)
         }
     }
+
 }
 
 #Preview {
